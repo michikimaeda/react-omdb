75c797f2187bbe74f0b09ea269d6aceaf573606b
Michiki Maeda <michikimaeda@gmail.com>
2023-01-25 03:17:04 +0900

    feat:App.tsxを、useReducerを使用したものに書き換え。

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Movie from './components/Movie';
@@ -7,40 +7,88 @@ import { MovieProps } from './components/Movie';
 
 const MOVIE_API_URL = 'https://www.omdbapi.com/?s=man&apikey=4a3b711b';
 
-const App: React.FC = () => {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+type State = {
+  loading: boolean;
+  movies?: MovieProps[];
+  errorMessage?: any;
+};
 
+type Action = {
+  type:
+    | 'SEARCH_MOVIES_REQUEST'
+    | 'SEARCH_MOVIES_SUCCESS'
+    | 'SEARCH_MOVIES_FAILURE';
+  payload?: any;
+  error?: any;
+};
+
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null,
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'SEARCH_MOVIES_REQUEST':
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null,
+      };
+    case 'SEARCH_MOVIES_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload,
+      };
+    case 'SEARCH_MOVIES_FAILURE':
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error,
+      };
+    default:
+      return state;
+  }
+};
+
+const App: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(() => {
     fetch(MOVIE_API_URL)
       .then((response) => response.json())
       .then((jsonResponse) => {
-        setMovies(jsonResponse.Search);
-        setLoading(false);
+        dispatch({
+          type: 'SEARCH_MOVIES_SUCCESS',
+          payload: jsonResponse.Search,
+        });
       });
   }, []);
 
   const search = (searchValue: string) => {
-    setLoading(true);
-    setErrorMessage(null);
+    dispatch({
+      type: 'SEARCH_MOVIES_REQUEST',
+    });
 
     fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
       .then((response) => response.json())
       .then((jsonResponse) => {
         if (jsonResponse.Response === 'True') {
-          setMovies(jsonResponse.Search);
-          setLoading(true);
+          dispatch({
+            type: 'SEARCH_MOVIES_SUCCESS',
+            payload: jsonResponse.Search,
+          });
         } else {
-          setErrorMessage(jsonResponse.Error);
-          setLoading(false);
+          dispatch({
+            type: 'SEARCH_MOVIES_FAILURE',
+            error: jsonResponse.Error,
+          });
         }
-      })
-      .catch((error) => {
-        setErrorMessage(error);
-        setLoading(false);
       });
   };
+
+  const { movies, errorMessage, loading } = state;
 
   return (
     <div className="App">
@@ -53,7 +101,7 @@ const App: React.FC = () => {
         ) : errorMessage ? (
           <div className="errorMessage">{errorMessage}</div>
         ) : (
-          movies.map((movie, index) => (
+          movies.map((movie: MovieProps, index: number) => (
             <Movie key={`${index}-${movie.Title}`} {...movie} />
           ))
         )}
